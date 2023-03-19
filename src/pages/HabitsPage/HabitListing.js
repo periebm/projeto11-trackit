ee7d98e6ee15845d2ead012ab97e4d1eb72a5482
Peri Note <periclesebmendes@gmail.com>
2023-03-19 00:01:21 -0300

    delete habit

--- src/pages/HabitsPage/HabitListing.js
+++ src/pages/HabitsPage/HabitListing.js
@@ -1,32 +1,53 @@
 import { DaysButton, WeekdaysContainer, HabitContainer } from "./styled"
 import { weekDays } from "../../constants/constants"
+import axios from "axios"
+import { userContext } from "../../context/userContext"
+import { useContext } from "react"
 
+export default function HabitListing({ habitsList, deleteFlag, setDeleteFlag }) {
+    const { token, URL } = useContext(userContext)
 
-export default function HabitListing({habitsList}) {
+    function deleteHabit(id) {
+        const deleteConfirm = window.confirm("Você tem certeza que quer deletar este hábito?")
+        if (deleteConfirm) {
+            deleteRequest(id)
+        } else return
+    }
+
+    function deleteRequest(id) {
+        axios.delete(`${URL}/habits/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+            .then((result) => {
+                setDeleteFlag(!deleteFlag);
+            })
+            .catch((err) => {
+                alert(err.response.data.message)
+            });
+    }
+
     return (
-        <> 
-        {
-            habitsList.map((h) => (
-                <HabitContainer data-test="habit-container" key={h.id}>
-                    <h4 data-test="habit-name">{h.name}</h4>
-
-                    {h.days !== undefined &&
-                        <WeekdaysContainer>
-                            {weekDays.map((d, i) => (
-                                <DaysButton
-                                    data-test="habit-day"
-                                    daySelected={(h.days).includes(i)}
-                                    key={i}
-                                >
-                                    {d}
-                                </DaysButton>))}
-                        </WeekdaysContainer>}
+        <>
+            {
+                habitsList.map((h) => (
+                    <HabitContainer data-test="habit-container" key={h.id}>
+                        <h4 data-test="habit-name">{h.name}</h4>
 
 
-                    <ion-icon name="trash-outline"></ion-icon>
-                </HabitContainer>
-            ))
-        }
+                        {h.days !== undefined &&
+                            <WeekdaysContainer>
+                                {weekDays.map((d, i) => (
+                                    <DaysButton
+                                        data-test="habit-day"
+                                        daySelected={(h.days).includes(i)}
+                                        key={i}
+                                    >
+                                        {d}
+                                    </DaysButton>))}
+                            </WeekdaysContainer>}
+
+                        <ion-icon onClick={() => deleteHabit(h.id)} name="trash-outline"></ion-icon>
+                    </HabitContainer>
+                ))
+            }
         </>
     )
 }
--- src/pages/HabitsPage/HabitsPage.js
+++ src/pages/HabitsPage/HabitsPage.js
@@ -13,29 +13,28 @@ export default function HabitsPage() {
         headers: { Authorization: `Bearer ${token}` }
     }
     const [selectedDays, setSelectedDays] = useState([]);
+    const [deleteFlag, setDeleteFlag] = useState(false);
+
 
     useEffect(() => {
         axios.get(`${URL}/habits`, config)
             .then((result) => {
-                //console.log(result.data)
                 setHabitsList(result.data)
             })
             .catch((err) => {
                 alert(err.response.data.message)
             });
 
-    }, [addHabitFlag])
+    }, [addHabitFlag, deleteFlag])
 
 
     function daysSelection(i) {
         if (selectedDays.includes(i)) {
             const newSelectedDays = selectedDays.filter(day => day !== i)
             setSelectedDays(newSelectedDays)
-            console.log(newSelectedDays)
         } else {
             const newSelectedDays = [...selectedDays, i]
             setSelectedDays(newSelectedDays)
-            console.log(newSelectedDays)
         }
     }
 
@@ -56,6 +55,8 @@ export default function HabitsPage() {
 
                 <HabitListing
                     habitsList={habitsList}
+                    setDeleteFlag={setDeleteFlag}
+                    deleteFlag={deleteFlag}
                 />
 
                 {habitsList.length === 0 && (<p> Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>)}
